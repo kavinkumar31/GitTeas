1b8c9633512619b5470011b0861ec5e06eb17de7
kavinkumar31 <kavinkumarduraisamy31@gmail.com>
2023-01-23 08:30:01 +0530

    Task Done

---
+++ src/test/java/org/git/Gitclass.java
@@ -0,0 +1,16 @@
+package org.git;
+
+public class Gitclass {
+	
+private void Class1() {
+	System.out.println(1);
+
+}
+private void class2() {
+	System.out.println(2);
+
+}
+private void class3() {
+	System.out.println(3);
+}
+}
